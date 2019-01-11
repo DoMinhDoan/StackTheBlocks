3c2cb9b5131b977c6d5e3808a7c76aaccc5ed9d8
doan.dominh <doan.dominh@gameloft.com>
2019-01-11 17:48:11 +0700

    - moving and split cube

---
+++ Assets/Scripts/MovingCube.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCube : MonoBehaviour
+{
+    public float moveSpeed = 1.0f;
+
+    public static MovingCube CurrentCube { get; private set; }
+    public static MovingCube LastCube { get; private set; }
+
+    void OnEnable()
+    {
+        if(LastCube == null)
+        {
+            LastCube = GameObject.Find("Start").GetComponent<MovingCube>();
+        }
+        CurrentCube = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.forward * Time.deltaTime * moveSpeed;        
+    }
+
+    internal void Stop()
+    {
+        moveSpeed = 0;
+
+        float hangover = transform.position.z - LastCube.transform.position.z;
+
+        SplitCubeOnZ(hangover);
+    }
+
+    private void SplitCubeOnZ(float hangover)
+    {
+        float newZSize = LastCube.transform.localScale.z - Mathf.Abs(hangover);
+        float fallingBlockSize = transform.localScale.z - newZSize;
+
+        float newZPosition = LastCube.transform.position.z + (hangover / 2);
+
+        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, newZSize);
+        transform.position = new Vector3(transform.position.x, transform.position.y, newZPosition);
+    }
+}
